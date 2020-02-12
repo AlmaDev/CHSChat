90045bf9b6b4965a91ee479c340a8a076cae95f0
Glitch (chschatroom) <none>
2020-02-12 12:22:21 +0000

    V 1.0

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
+const app = require("express")();
+const SocketAntiSpam = require("socket-anti-spam");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
+
+app.get("/chat?", (req, res) => {
+  // res.sendFile(__dirname  + "/error.html");
+  res.redirect("/")
+})
 
 app.post("/chat", (req, res) => {
-  res.cookie('name', req.body.username);
-  res.sendFile(__dirname + '/index.html');
+  res.cookie("name", req.body.username);
+  res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/admin", (req,res) => {
+  if(req.param("code") === "Marco>Damir") {
+    res.sendFile(__dirname + "/admin.html")
+  }
+})
+
 function getCookie(cookie) {
-  var cookiearray = cookie.split('=');
+  var cookiearray = cookie.split("=");
   for (var i = 0; i < cookiearray.length; i++) {
-    if (cookiearray[i].includes('name')) {
+    if (cookiearray[i].includes("name")) {
       return cookiearray[i + 1].split(" ")[0].replace(";", "");
     }
   }
 }
 
-
-io.sockets.on('connection', function (socket) {
-  var name = getCookie(socket.handshake.headers['cookie']);
-  socket.emit("chat message", `User ${name} has joined the chat`)
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', name + ":   " + msg);
+io.sockets.on("connection", socket => {
+  var name = getCookie(socket.handshake.headers["cookie"]);
+  io.emit("chat message", `User ${name} has joined the chat`);
+  socket.emit('join', name);
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", name + ": " + msg);
   });
 });
 
-http.listen(port, function () {
-  console.log('listening on *:' + port);
+http.listen(port, () => {
+  console.log("listening on *:" + port);
 });
